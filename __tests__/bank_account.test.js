c28affeee53bea0ec0d46434daf935b9da73b245
Sam Jones <samueldjones24@gmail.com>
2019-03-04 20:46:35 +0000

    Added overdraftLimit

--- __tests__/bank_account.test.js
+++ __tests__/bank_account.test.js
@@ -1,6 +1,6 @@
 const CURRENT_ACCOUNT = require('../src/bank_account');
 
-describe('deposit money into a current account', () => {
+describe('deposit money into current account', () => {
 
     test('show current account balance', () => {
         const MY_ACCOUNT = new CURRENT_ACCOUNT();
@@ -15,7 +15,7 @@ describe('deposit money into a current account', () => {
     })
 });
 
-describe('withdraw money from a current account', () => {
+describe('withdraw money from current account', () => {
 
     test('show current account balance', () => {
             const MY_ACCOUNT = new CURRENT_ACCOUNT();
@@ -37,5 +37,14 @@ describe('check balance of current account', () => {
             const MY_ACCOUNT = new CURRENT_ACCOUNT();
             MY_ACCOUNT.balance = 1200;
             expect(MY_ACCOUNT.checkBalance()).toBe(1200);
+    })
+});
+
+describe('overdraft limit', () => {
+
+    test('throws an error if you withdraw more than your overdraft limit', () => {
+            const MY_ACCOUNT = new CURRENT_ACCOUNT();
+            MY_ACCOUNT.balance = -250;
+            expect(() => MY_ACCOUNT.withdraw(100)).toThrow('Sorry, you have insufficient funds.');
     })
 });
--- src/bank_account.js
+++ src/bank_account.js
@@ -1,4 +1,7 @@
 
+const OVERDRAFT_LIMIT = -300;
+
+
 function CURRENT_ACCOUNT() {
     this.balance = 0;
 }
@@ -8,6 +11,9 @@ CURRENT_ACCOUNT.prototype.deposit = function(amount) {
 };
 
 CURRENT_ACCOUNT.prototype.withdraw = function(amount) {
+    if(this.balance - amount < OVERDRAFT_LIMIT) {
+        throw new Error('Sorry, you have insufficient funds.')
+    } 
     this.balance -= amount;
 };
 
